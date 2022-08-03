01847f72a646e2e7a49545988ec52089bad50b2a
Jewel Osafile <osafilejewel@gmail.com>
2022-08-03 18:45:38 +0100

    homescreen header

---
+++ src/HomeScreen.js
@@ -0,0 +1,56 @@
+import React, {useLayoutEffect} from 'react'
+import { SafeAreaView, View, Text, Platform, StatusBar, Image, TextInput } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import GlobalStyles from '../GlobalStyles'
+import {UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon } from 'react-native-heroicons/outline'
+
+const HomeScreen = () => {
+  const navigation = useNavigation()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      // headerTitle: 'Testingggg'
+      headerShown: false,
+    })
+  }, [navigation])
+
+  return (
+    <SafeAreaView className="bg-white pt-5" style={GlobalStyles.droidSafeArea}>
+      <>
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image 
+            source={{
+              uri: 'https://links.papareact.com/wru'
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View className='flex-1'>
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+            <Text className="font-bold text-xl">Current Location
+            <ChevronDownIcon  size={20} color="#00cc88" />
+             
+           </Text>
+          </View>
+            <UserIcon size={35} color="#00cc88" />
+        
+        </View>
+        {/* Search */}
+
+        <View className="flex-row items-center space-x-2 pb-2 mx-4">
+          <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+            <SearchIcon color="gray" size={20} />
+            <TextInput placeholder='Restaurants and cuisines'
+                    keyboardType='default'
+            />
+          </View>
+            <AdjustmentsIcon color="#00cc88" />
+        </View>
+      </>
+      
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
