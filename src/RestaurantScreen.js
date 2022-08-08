fcf0e260e356f5da44d6ef13b369cfd0f0f29bcb
Jewel Osafile <osafilejewel@gmail.com>
2022-08-08 13:20:30 +0100

    restaurantscreen

--- src/RestaurantScreen.js
+++ src/RestaurantScreen.js
@@ -2,8 +2,9 @@ import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import {useNavigation, useRoute, UseRoute} from '@react-navigation/native'
 import { urlFor } from '../sanity'
-import { ArrowLeftIcon } from 'react-native-heroicons/outline'
-import { StarIcon } from 'react-native-heroicons/solid'
+import { ArrowLeftIcon, QuestionMarkCircleIcon } from 'react-native-heroicons/outline'
+import { ChevronRightIcon, LocationMarkerIcon, StarIcon } from 'react-native-heroicons/solid'
+import DishRow from '../components/DIshRow'
 
 
 const RestaurantScreen = () => {
@@ -43,13 +44,48 @@ const RestaurantScreen = () => {
             <View className="px-4 pt-4">
                 <Text className="text-3xl font-bold">{title}</Text>
                 <View className="flex-row space-x-2 my-1">
-                    <View>
+                    <View className="flex-row items-center space-x-1">
                         <StarIcon size={22} color="green" opacity={0.5} />
                         <Text className="text-xs text-gray-500">
                             <Text className="text-green-500">{rating}</Text> .{genre}
                         </Text>
                     </View>
+                   
+                    <View className="flex-row items-center space-x-1">
+                        <LocationMarkerIcon size={22} color="gray" opacity={0.4} />
+                        <Text className="text-xs text-gray-500">
+                            <Text className="text-green-500">Nearby . {address}</Text>
+                        </Text>
+                    </View>
+
                 </View>
+                <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+            </View>
+            <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+                <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+                <Text className="pl-2 flex-1 text-md font-bold">
+                    Have a food allergy?
+                </Text>
+                <ChevronRightIcon color="#00cc88" />
+            </TouchableOpacity>
+            <View>
+                <Text className="px-4 pt-5 mb-3 font-bold text-xl">
+                    Menu
+                </Text>
+
+            {/* DishRows */}
+             {dishes.map(dish => (
+                <DishRow 
+                    key={dish._id}
+                    id={dish._id}
+                    name={dish.name}
+                    description={dish.short_description}
+                    price={dish.price}
+                    image={dish.image}
+                />
+             ))}
+
+
             </View>
         </View>
       
